46d426d0977c5ffec57b0b991ce5dc7087c46368
Javier Siñani Lijeron <a2019113929@estudiantes.upsa.edu.bo>
2021-04-15 15:38:27 -0400

    se agrego el proyecto web MVC

---
+++ Persona/apiPersona/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace apiPersona.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConecction")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<apiPersona.Models.Person> People { get; set; }
+    }
+}
