82b5d42e07313d2852aa2c7c9fe4fdbac03c30aa
Javier Siñani Lijeron <a2019113929@estudiantes.upsa.edu.bo>
2021-04-15 15:47:52 -0400

    se agrego controlador people

---
+++ Persona/WebPersona/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebPersona.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConecction")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<WebPersona.Models.Person> People { get; set; }
+    }
+}
